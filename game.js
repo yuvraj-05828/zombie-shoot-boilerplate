52a46f483fc010c7b75b5eedce1e29cebccdcaa4
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2022-10-27 23:53:42 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,19 @@
+// Iteration 1: Declare variables required for this game
+
+// Iteration 1.2: Add shotgun sound
+
+// Iteration 1.3: Add background sound
+
+// Iteration 1.4: Add lives
+
+// Iteration 2: Write a function to make a zombie
+
+// Iteration 3: Write a function to check if the player missed a zombie
+
+// Iteration 4: Write a function to destroy a zombie when it is shot or missed
+
+// Iteration 5: Creating timer
+
+// Iteration 6: Write a code to start the game by calling the first zombie
+
+// Iteration 7: Write the helper function to get random integer
